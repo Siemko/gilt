e0df279460caea0c2a094692b36da9926256aede
Dominik <dom.guzy@gmail.com>
2018-09-09 20:50:44 +0200

    dotnet backend

---
+++ backends/dotnet/gilt/Controllers/TranslateController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Localization;
+
+namespace gilt.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TranslateController : ControllerBase
+    {
+        private readonly IStringLocalizer<TranslateController> _localizer;
+
+        public TranslateController(IStringLocalizer<TranslateController> localizer)
+        {
+            _localizer = localizer;
+        }
+
+        // GET api/values
+        [HttpGet]
+        public ActionResult<string> Get()
+        {
+            return _localizer["Title"].Value;
+        }
+    }
+}
---
+++ backends/dotnet/gilt/Startup.cs
@@ -0,0 +1,58 @@
+﻿using System.Globalization;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Localization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Options;
+
+namespace gilt
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+
+            services.AddLocalization(options => options.ResourcesPath = "Resources");
+
+            services.Configure<RequestLocalizationOptions>(options =>
+            {
+                var supportedCultures = new[]
+                {
+                    new CultureInfo("en"),
+                    new CultureInfo("it"),
+                    new CultureInfo("pl")
+                };
+                options.DefaultRequestCulture = new RequestCulture("pl");
+                options.SupportedCultures = supportedCultures;
+                options.SupportedUICultures = supportedCultures;
+                options.RequestCultureProviders.Insert(0, new QueryStringRequestCultureProvider());
+            });
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            var locOptions = app.ApplicationServices.GetService<IOptions<RequestLocalizationOptions>>();
+            app.UseRequestLocalization(locOptions.Value);
+
+            if (env.IsDevelopment())
+                app.UseDeveloperExceptionPage();
+            else
+                app.UseHsts();
+
+            app.UseHttpsRedirection();
+            app.UseMvc();
+        }
+    }
+}
